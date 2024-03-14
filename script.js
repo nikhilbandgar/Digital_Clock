d00199dd06939f4ec6b898d03f46416de8b70c52
nikhilbandgar <30629877+nikhilbandgar@users.noreply.github.com>
2024-03-14 20:12:33 +0530

    First Upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const clock = document.getElementById('MyClockDisplay');
+
+
+
+
+setInterval(() => {
+    let date = new Date();
+    console.log(date.toLocaleTimeString());
+    clock.innerText = date.toLocaleTimeString()
+}, 1000);
